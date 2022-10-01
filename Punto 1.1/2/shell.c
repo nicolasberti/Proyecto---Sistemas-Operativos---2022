90c010e85477eb8846277dbc708c498d30ace23f
Nicolás Berti <71272720+nicolasberti@users.noreply.github.com>
2022-10-01 16:30:41 -0300

    Update shell.c

--- Punto 1.1/2/shell.c
+++ Punto 1.1/2/shell.c
@@ -13,8 +13,6 @@
 #define ERROR_PIPE					    	1
 #define ERROR_FORK              			2
 
-
-
 /* Errores pero para imprimir por consola */
 #define ERROR_LIMITE 				     	printf("~$ Limite de comandos");
 #define	ERROR_DECOMANDO				     	printf("~$ El comando no pudo ser ejecutado [revise los parametros]");
